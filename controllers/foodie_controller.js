8791d97afb608aa54920e894310f8aa7f73bd163
Keisha <kmmatthews15@gmail.com>
2020-01-30 20:16:49 -0600

    added mroe routes and completed the foodie controller page

--- controllers/foodie_controller.js
+++ controllers/foodie_controller.js
@@ -5,5 +5,49 @@ const isAuth = require("../config/middleware/isAuth");
 const db = require("../models");
 
 module.exports = function (app) {
-   // add a new recipe in the recipe book 
-}
+   // add a new recipe to the dashboard 
+   app.post("/recipe", function (req, res) {
+      req.body.recipebook = true;
+      db.User.create(req.body).then(function (dbUser) {
+         req.body.UserId = dbUser.id;
+         db.Recipe.create(req.body)
+            .then(function (dbRecipe) {
+               req.body.RecipeId = dbRecipe.id;
+               db.Description.create(req.body).then(function () {
+                  db.Instruction.create(req.body).then(function () {
+                     res.redirect(307, "index");
+                  });
+               });
+            })
+            .catch(function (err) {
+               res.status(500).json(err);
+            });
+      });
+   });
+
+   // returns all of the recipes 
+   app.get("/api/recipes", isAuth.isAuthenticated, function (req, res) {
+      db.Recipe.findAll({
+         attributes: {
+            exclude: ["ingredients", "UserId"]
+         },
+         include: [db.Description]
+      }).then(function (dbRecipe) {
+         res.json(dbRecipe);
+      });
+   });
+
+   // returns the recipes on the recipebook page
+   app.get("/profile/recipe/:id", isAuth.isAuthenticated, function (req, res) {
+      db.Recipe.findOne({
+         where: {
+            id: req.params.id
+         }
+      }).then(function (dbRecipe) {
+         let hbsObj = {
+            recipe: dbRecipe
+         };
+         res.render("recipebook", hbsObj);
+      });
+   });
+};
--- routes/html_routes.js
+++ routes/html_routes.js
@@ -1,7 +1,7 @@
 const isAuth = require("../config/middleware/isAuth");
 
 module.exports = function (app) {
-   // homepage (index)
+   // homepage (index)...this will have the initial page where you can either signin or take the quiz 
    app.get('/', function (req, res) {
       res.render('index');
    });
@@ -18,25 +18,34 @@ module.exports = function (app) {
       else res.redirect('/UserProfile');
    });
 
+   // renders the user profile page...or the dashboard
    app.get('/userProfile', function (req, res) {
       res.render('userProfile');
    });
 
    // renders the login page 
    app.get('/login', function (req, res) {
-      if (!req.user) 
+      if (!req.user)
          res.render('login');
       else res.redirect('/profile');
    });
 
+   // renders the about page 
    app.get('/about', function (req, res) {
       res.render('about');
    });
 
+   // renders the main dashboard 
+   app.get('/account', function (req, res) {
+      res.render('account');
+   })
+
+   // renders the recipe book page
    app.get('./recipebook', function (req, res) {
       res.render('recipebook');
    });
 
+   // renders  the search recipe page 
    app.get('./searchrecipe', function (req, res) {
       res.render('searchrecipe');
    })
